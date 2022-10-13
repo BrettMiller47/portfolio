5d2084972a0a6b1a4ab7fd8d2ce32facbaf50ace
Brett Miller <brett.miller47@yahoo.com>
2022-10-13 17:26:52 -0400

    fix route for home path

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Nav />
         <Routes>
           <Route 
-            path="/" 
+            path="/portfolio" 
             element={<Home />} 
           />
           <Route 
