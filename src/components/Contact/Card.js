be9df7bb0afd378aacbd82b514a2cc77cea0fa98
Brett Miller <brett.miller47@yahoo.com>
2022-10-31 19:17:49 -0400

    switched to .value for body variable due to textarea tag

--- src/components/Contact/Card.js
+++ src/components/Contact/Card.js
@@ -30,7 +30,7 @@ export default function Card() {
     e.preventDefault();
     let name = document.getElementById('name').textContent;
     let subject = 'CONTACT FORM message from ' + name;
-    let body = document.getElementById('body').textContent;
+    let body = document.getElementById('body').value;
     window.location.href = 'mailto:brett.miller47@yahoo.com?subject=' + subject + '&body=' + body;
     document.getElementById('contact-form').reset();
   }
