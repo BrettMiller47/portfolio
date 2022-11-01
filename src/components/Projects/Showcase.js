eb5fb145686ee45a5b019dcdfc34d08e9be7e63d
Brett Miller <brett.miller47@yahoo.com>
2022-11-01 19:54:53 -0400

    added MySQL workforce project

--- src/components/Projects/Showcase.js
+++ src/components/Projects/Showcase.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Card from '../Card/index';
-import arbPic from '../assets/images/arb.png'
+import arbPic from '../assets/images/arb.png';
+import workforceGif from '../assets/videos/workforceManager.gif';
 
 export default function ProjectShowcase() {
 
   const cards = [
+    {
+      header: 'MySQL Workforce Manager',
+      desc: 'A command-line application built with Inquirer and Sequelize.',
+      imgSrc: '',
+      iFrameSrc: workforceGif,
+      iFrameTitle: 'MySQL Workforce Manager',
+      projectSrc: 'https://github.com/BrettMiller47/sql-employee-tracker'
+    },
     {
       header: 'Excel VBA Automation',
       desc: 'A "How To..." video for teaching purposes.',
