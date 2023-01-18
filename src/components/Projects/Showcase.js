66ba6385fcf79b81349365d419b2044ce72691a5
Brett Miller <brett.miller47@yahoo.com>
2023-01-18 13:35:21 -0500

    added py-sql-example project

--- src/components/Projects/Showcase.js
+++ src/components/Projects/Showcase.js
@@ -7,10 +7,19 @@ import workforceGif from '../assets/videos/workforceManager.gif';
 import beantownGif from '../assets/videos/beantown.gif';
 import pourLoserPic from '../assets/images/pour-loser.png';
 import scraperPic from '../assets/images/command-line-scraper.png'
+import pySqlPic from '../assets/images/py-sql.png'
 
 export default function ProjectShowcase() {
 
   const cards = [
+    {
+      header: 'Python & SQL Database',
+      desc: 'CRUD operations using Python and MySQL.',
+      imgSrc: pySqlPic,
+      iFrameSrc: '',
+      iFrameTitle: '',
+      projectSrc: 'https://github.com/BrettMiller47/python-sql-example'
+    },
     {
       header: 'MySQL Workforce Manager',
       desc: 'A command-line application using Sequelize.',
