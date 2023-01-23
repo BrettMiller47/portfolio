e09a0a56e7a41ca8369189ca2d789fb898d82abc
Brett Miller <brett.miller47@yahoo.com>
2023-01-23 10:47:38 -0500

    removed linkedIn

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useTheme } from '../../utils/ThemeContext';
-import linkedin from '../assets/images/linkedin.png';
 import githubLight from '../assets/images/githubLight.png';
 import githubDark from '../assets/images/githubDark.png';
 
@@ -21,9 +20,6 @@ export default function Footer() {
     <div className='container-fluid d-flex justify-content-center' style={styles.footer}>
       <a href='https://github.com/brettmiller47' target='_blank' rel='noreferrer' className='m-2 p-0'>
         <img src={darkTheme ? githubDark : githubLight} alt='GitHub logo' height='64px'></img>
-      </a>
-      <a href='https://www.linkedin.com/in/brett-miller-5b8913100' target='_blank' rel='noreferrer' className='m-2 p-0' width="85px">
-        <img src={linkedin} alt='LinkedIn logo' height='64px'></img>
       </a>
     </div>
   );
